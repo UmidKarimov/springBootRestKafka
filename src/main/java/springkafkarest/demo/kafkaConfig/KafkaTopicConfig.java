3f3712a581e6f315ffb27e2eaaf39409050d7bad
UmidKarimov <Umid.Karimov@unibank.az>
2023-03-14 14:17:12 +0400

    add partitions and replicas

--- src/main/java/springkafkarest/demo/kafkaConfig/KafkaTopicConfig.java
+++ src/main/java/springkafkarest/demo/kafkaConfig/KafkaTopicConfig.java
@@ -2,12 +2,17 @@ package springkafkarest.demo.kafkaConfig;
 
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.internals.Topic;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
     public NewTopic myTopic(){
-        return TopicBuilder.name("employee-events").build();
+
+        TopicBuilder builder = TopicBuilder.name("employee-events");
+        builder.partitions(3);
+        builder.replicas(2);
+        return builder.build();
     }
 }
